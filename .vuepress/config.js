341457987b7ef883b45884d57c770800589acc8d
mabel <js@yababay.ru>
2019-01-13 23:39:01 +0300

    bio-2003

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    description: 'Материалы к автобиографии',
+    title: 'О времени и о себе',
+    themeConfig: {
+        sidebar: [
+            '/',
+            '/bio/',
+            '/valeriada/'
+        ]
+    }
+}
